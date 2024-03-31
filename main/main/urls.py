6b8132de65416f59948552661d10c7898df40370
Macsauce <rasheedbabatunde76@gmail.com>
2024-03-31 22:54:27 +0100

    Admin issue

--- main/main/urls.py
+++ main/main/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('/almighty/admin/', admin.site.urls),
+    path('almighty/admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
           name='schema-swagger-ui'),
     path('', schema_view.with_ui('swagger', cache_timeout=0),
