7b616404a31dd12ec773698c32674fd7d3fe2112
Macsauce <rasheedbabatunde76@gmail.com>
2024-03-31 02:12:23 +0100

    resolve host

--- main/main/settings.py
+++ main/main/settings.py
@@ -76,9 +76,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'main.urls'
+
 
 TEMPLATES = [
     {
